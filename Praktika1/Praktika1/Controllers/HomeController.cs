0d61ccf9f8e9993c1646c4e43ef07a4006305cc6
pupil <pupil@ivkhk.ee>
2020-09-07 15:14:14 +0300

    ASP web party

---
+++ Praktika1/Praktika1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Praktika1.Models;
+
+namespace Praktika1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string name="World!";
+            ViewBag.Hello = "Hello," + name;
+
+            int hour=DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Tere hommikust" : "Tere paevast";
+
+            return View();
+        }
+
+        public string Start()
+        {
+            return "Hello, World!";
+        }
+
+        [HttpGet]//Show form
+        public ViewResult RegisterForm()
+        {
+
+            return View();
+        }
+        [HttpPost]//Check form
+        public ViewResult RegisterForm(QuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+
+    }
+}
