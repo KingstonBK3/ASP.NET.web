f0922d67c49cea6a9dc0ff3e742c4b27129c53e2
pupil <pupil@ivkhk.ee>
2020-09-28 14:56:15 +0300

    menu is done

---
+++ PTVR18_Painters/MyPainterJPTVR18/Controllers/PaintersMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyPainterJPTVR18.Models;
+
+namespace MyPainterJPTVR18.Controllers
+{
+    public class PaintersMenuController : Controller
+    {
+        private PainterContext db = new PainterContext();
+
+        // GET: PaintersMenu
+        public ActionResult Index()
+        {
+            return View(db.Painters.ToList());
+        }
+
+        [ChildActionOnly]
+        public ActionResult PaintersMenu()
+        {
+            var paintersName = db.Painters.ToList();
+            return PartialView(paintersName);
+        }
+
+        public ActionResult Browse(int id)
+        {
+            var painterPictures = db.Painters.Include("Pictures").Single(g => g.Id == id);
+            return View(painterPictures);
+        }
+    }
+}
