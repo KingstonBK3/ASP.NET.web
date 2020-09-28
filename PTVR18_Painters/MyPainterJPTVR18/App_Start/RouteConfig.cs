6df6084bd5e85f58e0ace40c749fdb7a04efe29a
pupil <pupil@ivkhk.ee>
2020-09-28 14:03:18 +0300

    Painters project is done

--- PTVR18_Painters/MyPainterJPTVR18/App_Start/RouteConfig.cs
+++ PTVR18_Painters/MyPainterJPTVR18/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyPainterJPTVR18
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Painters", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
