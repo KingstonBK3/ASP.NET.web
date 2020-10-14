9cf9b35adec86c363cbe833bde6d53d3b23e10bf
pupil <pupil@ivkhk.ee>
2020-10-14 11:11:17 +0300

    Films project.Need to add getImage in method and hide ImageType

---
+++ FilmsProjectPTVR18/FilmsProjectPTVR18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmsProjectPTVR18.Startup))]
+namespace FilmsProjectPTVR18
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
